54693c1ef74c0edf6055bcb3b1129e2b414a763a
anvarov <anvarov9989@gmail.com>
2022-01-08 16:36:43 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var elHeding = document.querySelector(".heading");
 
-alert("Asalom alekum")
-alert ("Sayoxatingiz ucun sorovnomani toldiring")
+alert("Asalomu alekum")
+alert ("Sayoxatingiz uchun sorovnomani toldiring")
 
-var userName=prompt("imsingizni kiriting")
+var userName=prompt("Ismingizi  kiriting")
 
 var moution=750*9434+120*10355;
 
-var money=prompt(userName + " qancha pul sarflamoxchisiz")
+var money=prompt(userName + " Qancha pul sarflamoqchisiz")
 
 if (money>=moution){
-    elHeding.textContent="oqyol yaxwi borip kelin"
+    elHeding.textContent="Oq yol yaxwi borib keling"
 }
 else{
-    elHeding.textContent="pulingiz yetmadi kengi yilga xabar olin"
+    elHeding.textContent="Pulingiz yetmadi kengi yilga xabar oling"
 }
